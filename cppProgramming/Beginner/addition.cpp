d5c871c791d38e32d6eba07da828db9a765341b2
Yash Patil <63182838+yashpatilofficial@users.noreply.github.com>
2022-01-24 10:18:47 +0530

    comments updated

--- cppProgramming/Beginner/addition.cpp
+++ cppProgramming/Beginner/addition.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main()
 {
     int a,b,c;
-    cout<<"ENTER THE TWO NUMBERS:";
-    cin>>a>>b;
-    c=a+b;
-    cout<<"THE SUM IS ="<<c<<endl;
+    cout<<"ENTER THE TWO NUMBERS:"; //print instructions to user
+    cin>>a>>b; //takes var a and b as inputs from user
+    c=a+b; //addition operation
+    cout<<"THE SUM IS ="<<c<<endl; //var c is printed
 }
